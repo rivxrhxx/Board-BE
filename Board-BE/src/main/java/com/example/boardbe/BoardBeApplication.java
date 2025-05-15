8bfcd95e1db12d151151993c541f340d27af43b6
Kanghee <kh63167574@gmail.com>
2025-05-15 22:10:11 +0900

    Board

---
+++ Board-BE/src/main/java/com/example/boardbe/BoardBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardBeApplication.class, args);
+    }
+
+}
---
+++ Board-BE/src/test/java/com/example/boardbe/BoardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
